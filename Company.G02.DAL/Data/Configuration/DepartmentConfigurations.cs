7e6c2ecee06d29bf810e2480d974da0a26662c03
habibaahmedabdelmalek@outlook.sa <habibaahmedabdelmalek@outlook.sa>
2025-09-22 07:09:50 +0300

    Implementation of DAL

---
+++ Company.G02.DAL/Data/Configuration/DepartmentConfigurations.cs
@@ -0,0 +1,18 @@
+﻿using Company.G02.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.G02.DAL.Data.Configuration
+{
+    internal class DepartmentConfigurations : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Department> builder)
+        {
+            builder.Property(D => D.Id).UseIdentityColumn(10, 10);
+        }
+    }
+}
---
+++ Company.G02.DAL/Data/Contexts/CompanyDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Company.G02.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.G02.DAL.Data.Contexts
+{
+    internal class CompanyDbContext : DbContext
+    {
+        public CompanyDbContext() : base()
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = .;Database = CompanyG02 ; Trusted_connection = True;TrustServerCertificate = True");
+        }
+
+        public DbSet<Department> Departments { get; set; }
+    }
+}
