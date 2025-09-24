2503eb8a36d6bb61a9d411e084ac59dffe0e7432
habibaahmedabdelmalek@outlook.sa <habibaahmedabdelmalek@outlook.sa>
2025-09-24 12:25:19 +0300

    V10

--- Company.G02.PL/Controllers/DepartmentController.cs
+++ Company.G02.PL/Controllers/DepartmentController.cs
@@ -1,4 +1,6 @@
 ﻿using Company.G02.BLL.Repositories;
+using Company.G02.DAL.Models;
+using Company.G02.PL.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company.G02.PL.Controllers
@@ -22,5 +24,34 @@ namespace Company.G02.PL.Controllers
 
             return View(departments);
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateDepartmentDto model)
+        {
+            if(ModelState.IsValid) // Server Side Validation
+            {
+                var department = new Department()
+                {
+                    Code = model.Code,
+                    Name = model.Name,
+                    CreateAt = model.CreateAt
+                };
+                var count = _departmentRepository.Add(department);
+                if(count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+
+            return View(model);
+        }
+
+
     }
 }
