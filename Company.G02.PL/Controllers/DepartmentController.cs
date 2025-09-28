737a2b3a4fb5a9842d6f0817075338b8c8675bed
habibaahmedabdelmalek@outlook.sa <habibaahmedabdelmalek@outlook.sa>
2025-09-28 10:30:05 +0300

    V02

--- Company.G02.PL/Controllers/DepartmentController.cs
+++ Company.G02.PL/Controllers/DepartmentController.cs
@@ -2,6 +2,7 @@
 using Company.G02.DAL.Models;
 using Company.G02.PL.Dtos;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.DotNet.Scaffolding.Shared.Messaging;
 
 namespace Company.G02.PL.Controllers
 {
@@ -50,6 +51,18 @@ namespace Company.G02.PL.Controllers
             }
 
             return View(model);
+        }
+
+
+        [HttpGet]
+        public IActionResult Details(int? id)
+        {
+            if (id is null) return BadRequest("Invalid Id"); // 400
+
+            var department = _departmentRepository.Get(id.Value);
+            if(department is null) return NotFound(new { StatusCode = 404, Message = $"Department With This Id :{id} is not found" });
+
+            return View(department);
         }
 
 
