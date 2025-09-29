05ff18192c802a2877d02871449ceb73379e7b9b
habibaahmedabdelmalek@outlook.sa <habibaahmedabdelmalek@outlook.sa>
2025-09-29 12:06:30 +0300

    V07

---
+++ Company.G02.BLL/Repositories/EmployeeRepository.cs
@@ -0,0 +1,46 @@
+﻿using Company.G02.BLL.Interfaces;
+using Company.G02.DAL.Data.Contexts;
+using Company.G02.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.G02.BLL.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly CompanyDbContext _context;
+
+        public EmployeeRepository(CompanyDbContext context)
+        {
+           _context = context;
+        }
+        public IEnumerable<Employee> GetAll()
+        {
+            return _context.Employees.ToList();
+        }
+        public Employee? Get(int id)
+        {
+            return _context.Employees.Find(id);
+        }
+        public int Add(Employee model)
+        {
+            _context.Employees.Add(model);
+            return _context.SaveChanges();
+        }
+        public int Update(Employee model)
+        {
+            _context.Employees.Update(model);
+            return _context.SaveChanges();
+        }
+
+        public int Delete(Employee model)
+        {
+            _context.Employees.Remove(model);
+            return _context.SaveChanges();
+        }
+
+    }
+}
